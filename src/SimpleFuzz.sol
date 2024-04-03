55b44f9b06a164e02d6f50f102a4b67c1781e7c7
wildanvin <danvin.jim@gmail.com>
2024-04-02 22:21:31 -0500

    stateful fuzzing

--- src/SimpleFuzz.sol
+++ src/SimpleFuzz.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.13;
 contract SimpleFuzz {
 
     uint256 public shouldAlwaysBeZero = 0;
-
     uint256 private hiddenValue = 0;
 
     function doStuff (uint256 data) public {
-        if (data == 1234){
+        // if (data == 1234){
+        //     shouldAlwaysBeZero = 1;
+        // }
+        if (hiddenValue == 5678) {
             shouldAlwaysBeZero = 1;
         }
+        hiddenValue = data;
     }
 }
--- test/FoundrySimpleFuzz.t.sol
+++ test/FoundrySimpleFuzz.t.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {SimpleFuzz} from "../src/SimpleFuzz.sol";
-
-contract FoundrySimpleFuzz is Test {
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+contract FoundrySimpleFuzz is StdInvariant, Test {
     SimpleFuzz public simpleFuzz;
 
     function setUp() public {
         simpleFuzz = new SimpleFuzz();
-        //targetContract(address(simpleFuzz));
+        targetContract(address(simpleFuzz));
     }
 
     function testSimpleDoStuff() public {
@@ -20,6 +20,11 @@ contract FoundrySimpleFuzz is Test {
     // Stateless fuzzing
     function testFuzzDoStuff(uint256 x) public {
         simpleFuzz.doStuff(x);
+        assert(simpleFuzz.shouldAlwaysBeZero() == 0);
+    }
+
+    //Stateful fuzzing aka invariant test:
+    function invariant_testAlwaysReturnsZero () public view {
         assert(simpleFuzz.shouldAlwaysBeZero() == 0);
     }
 }
